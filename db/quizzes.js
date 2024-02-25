47d4cfd51f4667c10be118021dce6b2b28a303b0
Samuel Masih <samuelmasih.sls777@gmail.com>
2024-02-25 17:39:03 +0530

    Add

--- db/quizzes.js
+++ db/quizzes.js
@@ -106,7 +106,56 @@ const quizzes = {
                 },
             ]
         },
-
+        //3rd quiz
+        {
+            id: uuid(),
+            category: "dc",
+            image: "https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20(56)(110).jpg",
+            title: "DC",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique quis nunc eget rutrum",
+            quiz: [
+                {
+                    id: uuid(),
+                    question: "What was Wonder Woman originally named?",
+                    options: [
+                        { id: uuid(), option: "Anita", isCorrect: false },
+                        { id: uuid(), option: "Suprema", isCorrect: true },
+                        { id: uuid(), option: "Mishita", isCorrect: false },
+                        { id: uuid(), option: "None", isCorrect: false }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "Which supervillain was once the Iranian Ambassador to the U.N.?",
+                    options: [
+                        { id: uuid(), option: "Wonder Man", isCorrect: false },
+                        { id: uuid(), option: "Superman", isCorrect: false },
+                        { id: uuid(), option: "The Joker", isCorrect: true },
+                        { id: uuid(), option: "Batman", isCorrect: false }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "What is batman's favorite food?",
+                    options: [
+                        { id: uuid(), option: "Chicken Salad", isCorrect: false },
+                        { id: uuid(), option: "Turkey Roast", isCorrect: false },
+                        { id: uuid(), option: "Goat Ribbs", isCorrect: false },
+                        { id: uuid(), option: "Mulligatawny soup", isCorrect: true }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "In how many seconds can Green Arrow shoot and reload?",
+                    options: [
+                        { id: uuid(), option: "Two and a half seconds", isCorrect: true },
+                        { id: uuid(), option: "Two seconds", isCorrect: false },
+                        { id: uuid(), option: "One seconds", isCorrect: false },
+                        { id: uuid(), option: "One and a half seconds", isCorrect: false }
+                    ]
+                },
+            ]
+        },
     ]
 }
 
